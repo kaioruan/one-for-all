bc77e5fef08b657c6fac2c3fdafe5f71c3ba4348
kaioruan <kaio.ruan@souunit.com.br>
2022-07-22 21:45:49 -0300

    Feat: create Table albums

--- desafio1.sql
+++ desafio1.sql
@@ -15,3 +15,26 @@ VALUES
   ('Freedie Shannon'),
   ('Tyler Isle'),
   ('Fog');
+
+CREATE TABLE SpotifyClone.albums (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album VARCHAR(30) NOT NULL,
+    artista_id INT NOT NULL,
+    ano_lancamento INT NOT NULL,
+    FOREIGN KEY (artista_id)
+        REFERENCES SpotifyClone.artistas (artista_id)
+)  ENGINE=INNODB;
+
+
+INSERT INTO SpotifyClone.albums (album, artista_id, ano_lancamento)
+VALUES
+  ('Envious', 1, 1990),
+  ('Exuberant', 1, 1993),
+  ('Hallowed Steam', 2, 1995),
+  ('Incandescent', 3, 1998),
+  ('Temporary Culture', 4, 2001),
+  ('Library of liberty', 4, 2003),
+  ('Chained Down', 5, 2007),
+  ('Cabinet of fools', 5, 2012),
+  ('No guarantees', 5, 2015),
+  ('Apparatus', 6, 2015);
